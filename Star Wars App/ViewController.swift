9f08c7a66e6051fc4c4fbb76302fd9ff62d46e4b
Student <student@jcysewski.local>
2015-11-19 11:05:22 -0600

    Initial Commit

---
+++ Star Wars App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Star Wars App
+//
+//  Created by jcysewski on 11/19/15.
+//  Copyright © 2015 jcysewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
